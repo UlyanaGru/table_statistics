e4d5586e851864e8fa03c340877b069f2254f8a9
UlyanaGru <giperbolicheskiisinys@gmail.com>
2025-08-20 07:34:01 +0300

    func for reading .yaml

--- table_statistics.java
+++ table_statistics.java
@@ -1,5 +1,17 @@
-#Выхзов библиотек
+//Вызов библиотек
 import java.io.*;
 import java.util.*;
 import java.nio.file.Paths;
 import java.nio.file.Files;
+//Используемые библиотеки
+public class TableStatistics {
+    //Функция для чтения .yaml
+    public static Map<String, Object> loadYaml(String filePath) throws IOException {
+        /**
+         *Функция для чтения .yaml
+         *@param filePath путь к файлу YAML
+         *@return Map с данными из YAML
+         */
+        Map<String, Object> data = new HashMap<>();
+        return data;
+    }
